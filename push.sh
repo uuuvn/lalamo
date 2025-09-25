fb8d66c86cb818465557bc71ae14b307a0b7ca71
uuuvn <uuuvn@proton.me>
2025-09-25 19:50:11 +0400

    private: env

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -euo pipefail
+
+# WCP="$(jj log --no-graph -r @ -T 'parents.map(|c| "-d " ++ c.commit_id().short()).join(" ")')"
+
+if [ -n "$(jj diff --summary)" ]; then
+  jj squash --into private_env .envrc shell.nix .helix push.sh
+fi
+
+if [ -n "$(jj diff --summary)" ]; then
+  jj squash --into speculator
+fi
+
+for remote in "$@"; do
+  jj git push --config git.sign-on-push=false --remote "$remote" --allow-empty-description --allow-private --all --deleted
+  ssh "$remote" "cd src/lalamo && jj rebase -r @ -d private_env"
+done
